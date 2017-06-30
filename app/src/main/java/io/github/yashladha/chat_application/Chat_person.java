a180ec5003d08d759b0479f5ef51bd57cb84475d
yashLadha <yashladhapankajladha123@gmail.com>
2017-06-30 16:01:56 +0530

    Profile Image Option Added.

--- app/src/main/java/io/github/yashladha/chat_application/Chat_person.java
+++ app/src/main/java/io/github/yashladha/chat_application/Chat_person.java
@@ -1,15 +1,12 @@
 package io.github.yashladha.chat_application;
 
 import android.app.Activity;
-import android.app.NotificationManager;
-import android.content.Context;
 import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v7.app.NotificationCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -17,7 +14,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
--- app/src/main/java/io/github/yashladha/chat_application/Signup.java
+++ app/src/main/java/io/github/yashladha/chat_application/Signup.java
@@ -1,30 +1,53 @@
 package io.github.yashladha.chat_application;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.IOException;
 
 public class Signup extends Activity {
+
+    private int PICK_IMAGE_REQUEST = 1;
 
     private String TAG = getClass().getSimpleName();
     private EditText email;
     private EditText password;
     private Button Signup;
+    private Button ProfileImage;
+    private Uri profilImageUri;
 
     private FirebaseAuth mAuth;
+    private FirebaseStorage mStorage;
     private FirebaseAuth.AuthStateListener mAuthState;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri uri = data.getData();
+            profilImageUri = uri;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +57,10 @@ public class Signup extends Activity {
         email = (EditText) findViewById(R.id.email_signup);
         password = (EditText) findViewById(R.id.password_signup);
         Signup = (Button) findViewById(R.id.btnSignup);
+        ProfileImage = (Button) findViewById(R.id.btnProfile);
 
         mAuth = FirebaseAuth.getInstance();
+        mStorage = FirebaseStorage.getInstance();
         mAuthState = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -47,6 +72,16 @@ public class Signup extends Activity {
                 }
             }
         };
+
+        ProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+            }
+        });
 
         Signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,7 +96,23 @@ public class Signup extends Activity {
                         @Override
                         public void onSuccess(AuthResult authResult) {
                             Log.d(TAG, "Sign Up completed");
-                            finish();
+
+                            StorageReference profileImage = mStorage.getReference();
+                            profileImage = profileImage.child(authResult.getUser().getUid()+"/profileImage/"+profilImageUri.getLastPathSegment());
+
+                            UploadTask uploadTask = profileImage.putFile(profilImageUri);
+                            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                    Log.d(TAG, "Profile Image gets Uploaded");
+                                    finish();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(getApplicationContext(), "Profile Image Upload failed", Toast.LENGTH_SHORT).show();
+                                }
+                            });
                         }
                     }).addOnFailureListener(new OnFailureListener() {
                         @Override
